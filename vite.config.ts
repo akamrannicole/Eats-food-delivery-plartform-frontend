1b63e77549d13cdcad63063f87131269860a32fd
user.name <akamrannicole@gmail.com>
2024-06-18 21:52:26 +0300

    api

--- vite.config.ts
+++ vite.config.ts
@@ -10,12 +10,8 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    port: 5175, // Set your preferred port
-    hmr: {
-      protocol: 'ws://127.0.0.1:5175/', // Ensure the protocol is set correctly
-      host: '127.0.0.1',
-      port: 5175, // Ensure this matches the server port
-    },
+  build: {
+    outDir: 'dist', // Ensure this is set correctly
   },
+  base: '/', // Adjust if needed
 });
